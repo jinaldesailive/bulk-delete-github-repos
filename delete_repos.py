2576ea748d815fd6da95d6c47f8a9c1570fe02dc
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-07-30 11:30:49 +0100

    Create delete_repos.py

---
+++ delete_repos.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+token = '' # token needs delete_repo permission
+organization = 'monkmantra' # or user
+url = 'https://api.github.com/repos/{}'.format(organization)
+headers = {'Accept': 'application/vnd.github.v3+json',
+           'Authorization': 'token {}'.format(token)}
+
+
+lines = [line.strip() for line in open('todelete.txt')] # todelete.txt is a txt with repository names. One per line.
+for repo in lines:
+    print os.path.join(url, repo)
+    myrequest = requests.delete(os.path.join(url, repo), headers=headers)
+    print myrequest.content
