5c21b10e62e8f4b726a392ddbba6fd4a49315349
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-07-31 12:13:26 +0100

    updated

--- delete_repos.py
+++ delete_repos.py
@@ -15,5 +15,10 @@ lines = [line.strip() for line in open('todelete.txt')] # todelete.txt is a txt 
 
 for repo in lines:
     print(os.path.join(url, repo))
-    myrequest = requests.delete(os.path.join(url, repo), headers=headers)
-    print(myrequest.content)
+    myresponse = requests.delete(os.path.join(url, repo), headers=headers)
+    if myresponse.status_code == 204:
+        print(f"Successfully deleted {repo}.")
+    elif myresponse.status_code == 404:
+        print(f"Repository {repo} not found.")
+    else:
+        print(f"Failed to delete {repo}: {myresponse.status_code} {myresponse.text}")
